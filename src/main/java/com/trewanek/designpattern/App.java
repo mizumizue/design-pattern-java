46c2d71580060ca25f3b2ade870dea10b5e65f77
trewanek <trewanek@gmail.com>
2018-11-24 21:54:35 +0900

    Adapter Subclass mainに処理を追加

--- src/main/java/com/trewanek/designpattern/App.java
+++ src/main/java/com/trewanek/designpattern/App.java
@@ -4,11 +4,15 @@ import com.trewanek.designpattern.iterator.Book;
 import com.trewanek.designpattern.iterator.BookShelf;
 import com.trewanek.designpattern.iterator.Iterator;
 
+import com.trewanek.designpattern.adaptersubclass.Print;
+import com.trewanek.designpattern.adaptersubclass.PrintBanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
         quickStartIterator();
+        quickStartAdaptor();
     }
 
     public static void quickStartIterator() {
@@ -28,5 +32,11 @@ public class App
             Book book = (Book) i.next();
             System.out.println(book.getName());
         }
+    }
+
+    public static void quickStartAdaptor() {
+        Print print = new PrintBanner("Hello World!");
+        print.printWeak();
+        print.printStrong();
     }
 }
