d213aec6ec81ddd0e1007902b5875f18035fe610
trewanek <trewanek@gmail.com>
2018-11-24 21:53:45 +0900

    Adapter Subclass

---
+++ src/main/java/com/trewanek/designpattern/adaptersubclass/Print.java
@@ -0,0 +1,6 @@
+package com.trewanek.designpattern.adaptersubclass;
+
+public interface Print {
+    public void printWeak();
+    public void printStrong();
+}
---
+++ src/main/java/com/trewanek/designpattern/adaptersubclass/PrintBanner.java
@@ -0,0 +1,15 @@
+package com.trewanek.designpattern.adaptersubclass;
+
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    public void printWeak() {
+        showWithParen();
+    }
+
+    public void printStrong() {
+        showWithAster();
+    }
+}
